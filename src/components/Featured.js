875c0cf6f7ff2c66e6c8004df6fda63141002d6d
Nick Melms <nmelms92@gmail.com>
2022-08-28 14:41:28 -0400

    added featured parkes title and previous save

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -5,16 +5,17 @@ import FeaturedCard from "./FeaturedCard";
 export default function Featured() {
   const [featuredParks, setFeaturedParks] = useState([]);
   const { data, loading, setLoading } = useFetch(
-    `https://developer.nps.gov/api/v1/parks?limit=40&api_key=GwaTBYubTD2cu99IdYnM3NlKVj7HupkkxMxYU913`
+    `https://developer.nps.gov/api/v1/parks?limit=400&api_key=GwaTBYubTD2cu99IdYnM3NlKVj7HupkkxMxYU913`
   );
   function rand(max) {
     Math.floor(Math.random() * (max + 1));
   }
 
   const parks = [];
+
   if (data)
     while (parks.length < 3) {
-      let num = Math.floor(Math.random() * (40 + 1));
+      let num = Math.floor(Math.random() * (400 + 1));
       if (!parks.includes(data.data[num])) {
         parks.push(data.data[num]);
         console.log(data.data[num]);
@@ -23,6 +24,7 @@ export default function Featured() {
 
   return (
     <div className="featured">
+      <h1>Featured Parks</h1>
       {parks.map((item) => {
         return (
           <FeaturedCard
